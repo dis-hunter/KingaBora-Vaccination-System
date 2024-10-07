728e9dedd3c0080bb25335c8668a90c95bd93af7
StacyAkinyi <stacy.akinyi@gmail.com>
2024-10-07 09:53:17 +0300

    child folder

---
+++ Child/schildvaccination.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="Header-Footer/Stylesheets/header-footer.css" />
+    <link rel="stylesheet" href="childvaccination.css" />
+    <script src="https://kit.fontawesome.com/7bc8f8d494.js" crossorigin="anonymous"></script>
+    <script src="Header-Footer/header-footer.js"></script> 
+    <title>Child Vaccination</title>
+  </head>
+  <body>
+    <special-header></special-header>
+    <main>
+        <div class="main">
+            <div class="one">
+                <ul>
+                <li><img src='' alt='Profile photo'></li>
+                <li><h3>Name:</h3></li>
+                <li><h3>Age:</h3></li>
+                <div class="two">
+                <li><h3><i class="fa-solid fa-pen-to-square"></i>Edit Profile</h3></li>
+                <li><h3><i class="fa-solid fa-phone"></i>Emergency Contact</h3></li>
+                </div>
+                </ul>
+
+            </div>
+            <hr>
+            <div class="one" id="middle">
+                <ul>
+                <li><a href="" >Overview</a></li>
+                <li><a href=" ">Appointments</a></li>
+                <li><a href=" ">Vaccination Summary</a></li>
+                <ul>
+            </div>
+            <div class="one">
+               <table>
+                <tr>
+                    <th>Vaccine</th>
+                    <th>Due Date</th>
+                    <th>Status</th>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+               </tr>
+               <tr>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+               </tr>
+               </table>
+               <h4><a href="path/to/your/file.ext" download><i class="fa-solid fa-download"></i>DOWNLOAD</a></h4>
+            </div>
+        </div>
+    </main>
+    <special-footer></special-footer>
+  </body>
+  </html>
